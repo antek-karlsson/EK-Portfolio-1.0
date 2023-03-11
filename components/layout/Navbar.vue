618e658f24b8c462eab7d39255e64d6a86607465
Antek <tonylearnscoding@gmail.com>
2023-03-11 21:52:30 +0100

    [fix] Implement show/hide navbar on scroll up/down

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar">
+  <div class="navbar" :class="{ hidden: !isNavVisible }">
     <div class="navbar__content">
       <div class="navbar__header">
         <div class="navbar__logo">
@@ -56,8 +56,8 @@
         </template>
       </ClientOnly>
     </div>
+    <div v-if="!isNavOpen" class="navbar__cover"></div>
   </div>
-  <div v-if="!isNavOpen" class="navbar__cover"></div>
 </template>
 
 <script setup lang="ts">
@@ -68,8 +68,21 @@ const isScreenMobile = useMediaQuery(breakpoints.xs);
 const route = useRoute();
 const currentRoute = ref();
 
+const isNavVisible = ref(true);
+const lastScrollPosition = ref(0);
+
 function toggleMobileNav() {
   isNavOpen.value = !isNavOpen.value;
+}
+
+function onScroll() {
+  const currentScrollPosition = window.scrollY;
+  if (currentScrollPosition < 0 || Math.abs(currentScrollPosition - lastScrollPosition.value) < 60) {
+    return;
+  }
+
+  isNavVisible.value = currentScrollPosition < lastScrollPosition.value;
+  lastScrollPosition.value = currentScrollPosition;
 }
 
 watch(
@@ -87,6 +100,14 @@ watch(currentRoute, () => {
 watch(isScreenMobile, () => {
   !isScreenMobile && isNavOpen.value === true ? (isNavOpen.value = false) : (isNavOpen.value = false);
 });
+
+onMounted(() => {
+  window.addEventListener('scroll', onScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', onScroll);
+});
 </script>
 
 <style lang="scss" scoped>
@@ -97,6 +118,8 @@ watch(isScreenMobile, () => {
   right: 0;
   background-color: $white;
   z-index: 2;
+  transform: translate3d(0, 0, 0);
+  transition: 0.3s all ease-out;
 
   &__content {
     width: 100%;
@@ -188,6 +211,10 @@ watch(isScreenMobile, () => {
       top: 138px;
     }
   }
+}
+
+.navbar.hidden {
+  transform: translate3d(0, -100%, 0);
 }
 
 .open {
