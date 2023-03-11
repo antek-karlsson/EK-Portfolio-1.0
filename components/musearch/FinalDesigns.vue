6b26a21a93057b63c38e2354633c093b1f396fb5
Antek <tonylearnscoding@gmail.com>
2023-03-11 16:27:12 +0100

    [EK-13] Create FinalDesigns.vue section

---
+++ components/musearch/FinalDesigns.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="final-designs">
+    <ProjectSection is-dark>
+      <h3 class="final-designs__title">{{ title }}</h3>
+      <div v-for="(section, sectionId) in subSections" :key="sectionId" class="final-designs__section">
+        <div v-if="section.subSections" class="final-designs__section-double">
+          <div v-for="(sub, subId) in section.subSections" :key="subId" class="final-designs__section-double-sub">
+            <ProjectSubSection :title="subId === 0 ? section.title : ''" is-dark>
+              <template v-if="sub.text" #textRegular>
+                <p>{{ sub.text }}</p>
+              </template>
+            </ProjectSubSection>
+            <div class="final-designs__section-double-sub-images">
+              <img
+                v-for="(subImg, subImgId) in sub.images"
+                :key="subImgId"
+                :src="`/img/musearch/${subImg.url}`"
+                :alt="subImg.alt"
+              />
+            </div>
+          </div>
+        </div>
+        <div v-else class="final-designs__section">
+          <ProjectSubSection :title="section.title" is-dark>
+            <template #textRegular>
+              <p>{{ section.text }}</p>
+            </template>
+          </ProjectSubSection>
+          <div class="final-designs__section-images">
+            <img v-for="(img, imgId) in section.images" :key="imgId" :src="`/img/musearch/${img.url}`" :alt="img.alt" />
+          </div>
+        </div>
+      </div>
+    </ProjectSection>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { content } from '@/api/data/content/projects/musearch';
+const { title, subSections } = content.finalDesigns;
+</script>
+
+<style lang="scss" scoped>
+.final-designs {
+  &__title {
+    @include section-title('dark');
+  }
+
+  &__section,
+  &__section-double,
+  &__section-double-sub {
+    @include flex(column, flex-start, center, 6.8rem);
+    max-width: 100%;
+  }
+
+  &__section-images,
+  &__section-double-sub-images {
+    align-self: center;
+    @include flex(row, center, flex-start, 1.2rem);
+    flex-wrap: wrap;
+
+    @include desktop {
+      align-self: center;
+      @include flex(row, center, flex-start, 1.6rem);
+      flex-wrap: wrap;
+    }
+
+    img {
+      max-width: 10.1rem;
+
+      @include tablet {
+        max-width: 17rem;
+      }
+      @include desktop {
+        max-width: 26.3rem;
+      }
+    }
+  }
+}
+</style>
