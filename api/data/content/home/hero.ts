1ae2bdbd24706d109491bdb81dda00b833e3b2a0
Antek <tonylearnscoding@gmail.com>
2023-03-14 20:58:38 +0100

    [fix] Update content in hero.ts

--- api/data/content/home/hero.ts
+++ api/data/content/home/hero.ts
@@ -1,5 +1,8 @@
 export const content = {
-  header: `Hello! I'm Ewelina.`,
+  header: {
+    greeting: `Hello!`,
+    text: `I'm Ewelina.`,
+  },
   main: `I’m a UX/UI Designer passionate about improving the experience of others and looking for new solutions.`,
   sub: `I believe in the power of design as a problem solving tool and an act of communication.`,
   linkText: 'Check out my work',
