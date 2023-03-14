161d84202a7b5d9181b5e956318b7dd3b8f548e7
Antek <tonylearnscoding@gmail.com>
2023-03-14 21:14:24 +0100

    [EK-16] Create pages/resume.vue

---
+++ pages/resume.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="resume">
+    <embed src="/img/resume.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" width="100%" height="100%" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.resume {
+  height: 100vh;
+}
+</style>
