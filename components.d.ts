cc393b9b956fa3456b72d884638d606e07d3d514
Antek <tonylearnscoding@gmail.com>
2023-03-14 21:02:24 +0100

    [EK-16] Add fitToScreen icon

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     'IconCarbon:close': typeof import('~icons/carbon/close')['default']
+    'IconCarbon:fitToScreen': typeof import('~icons/carbon/fit-to-screen')['default']
     'IconCarbon:menu': typeof import('~icons/carbon/menu')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
