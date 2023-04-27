ab80cb439e5df200028e0b4d684b9dca5a647352
Antek <tonylearnscoding@gmail.com>
2023-04-27 19:27:39 +0200

    [EK-17] Create rheumapp.vue page

---
+++ pages/projects/rheumapp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="rheumapp">
+    <RheumappCTA />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.rheumapp {
+  height: 100%;
+}
+</style>
