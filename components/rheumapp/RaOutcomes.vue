a8ef1cb344f4e40a1a7a974a73610d411f982e2c
Antek <tonylearnscoding@gmail.com>
2023-05-06 13:12:20 +0200

    [EK-25] Create RaOutcomes.vue component

---
+++ components/rheumapp/RaOutcomes.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="outcomes">
+    <ProjectSection>
+      <h3 class="outcomes__title">{{ title }}</h3>
+      <ProjectSubSection v-for="(section, sectionId) in subSections" :key="sectionId" :title="section.title">
+        <template #textRegular>
+          <p>{{ section.text }}</p>
+        </template>
+      </ProjectSubSection>
+    </ProjectSection>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { content } from '@/api/data/content/projects/rheumapp';
+
+const { title, subSections } = content.outcomes;
+</script>
+
+<style lang="scss" scoped>
+.outcomes {
+  &__title {
+    @include section-title;
+  }
+}
+</style>
