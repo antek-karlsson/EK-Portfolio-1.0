06c63a1e41933f66e00fdb0031d07b6f6d8ff8ee
Antek <tonylearnscoding@gmail.com>
2024-01-23 21:19:37 +0100

    [EK-28] Update hero text

--- api/data/content/home/hero.ts
+++ api/data/content/home/hero.ts
@@ -3,7 +3,7 @@ export const content = {
     greeting: `Hello!`,
     text: `I'm Ewelina.`,
   },
-  main: `I’m a UX/UI Designer passionate about improving the experience of others and looking for new solutions.`,
+  main: `I’m a Graphic & Web Designer passionate about improving the experience of others and looking for new solutions.`,
   sub: `I believe in the power of design as a problem solving tool and an act of communication.`,
   linkText: 'Check out my work',
   linkUrl: '/projects/',
